ea2795d00b6af0ab7181864897ef0bec4096662f
Felipe Gonçalves <felipegoncalves@Felipes-MacBook-Pro.local>
2023-08-24 13:37:55 -0600

    moved Entity, EntityDB and EntityAPI into its own file entity.go

--- internal/pkg/domain/daily-ids.go
+++ internal/pkg/domain/daily-ids.go
@@ -1,23 +1,5 @@
 package domain
 
-type Entity struct {
-	ID       int         `json:"id,omitempty" bson:"id,omitempty"`
-	ObjectId interface{} `json:"_id,omitempty" bson:"_id,omitempty"`
-	ImdbID   string      `json:"imdb_id,omitempty" bson:"imdb_id,omitempty"`
-}
-
-type EntityDB interface {
-	Get(id int) (*interface{}, error)
-	List(search string) ([]*interface{}, error)
-	Upsert(e *interface{}) error
-}
-
-type EntityAPI interface {
-	GetFromAPI(e *interface{}) error
-	// ListFromAPI(search string) ([]*interface{}, error)
-	// ListChangedFromAPI(search string) ([]*interface{}, error)
-}
-
 type MovieTvIndex struct {
 	ID            int    `json:"id,omitempty" bson:"id,omitempty"`
 	OriginalTitle string `json:"original_title,omitempty" bson:"original_title,omitempty"`
