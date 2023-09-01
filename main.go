3492340d1c038c1dbd034e43ff7b4cd0e5240f3f
Felipe Gonçalves <felipegoncalves@Felipes-MacBook-Pro.local>
2023-09-01 09:27:40 -0600

    added some basic routes to main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	////// ROUTES //////
+
+	http.HandleFunc("/movies", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Movies! %s", r.Method)
+	})
+
+	http.HandleFunc("/tvseries", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "TV Series!")
+	})
+
+	http.HandleFunc("/people", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "People!")
+	})
+
+	http.HandleFunc("/tvnetworks", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "TV Networks!")
+	})
+
+	fmt.Println("Listening on port 8080...")
+	http.ListenAndServe("localhost:8080", nil)
+
+}
